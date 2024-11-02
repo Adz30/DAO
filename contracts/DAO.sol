4c8c47ad53fe8a8f9720c0de3759eeac10ef98e7
adam <kaw94@hotmail.co.uk>
2024-11-02 10:53:26 +0000

    completed contract

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED 
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -21,47 +21,77 @@ contract DAO {
     uint256 public proposalCount;
     mapping(uint256 => Proposal) public proposals;
 
-    event Propose(
-        uint id,
-        uint256 amount,
-        address recipient,
-        address creator
-    );
+    event Propose(uint id, uint256 amount, address recipient, address creator);
+
+    event Vote(uint256 id, address investor);
+
+    event Finalize(uint256 id);
 
     constructor(Token _token, uint256 _quorum) {
         owner = msg.sender;
-        token = _token; 
+        token = _token;
         quorum = _quorum;
-        
     }
 
     receive() external payable {}
 
+    modifier onlyInvestor() {
+        require(token.balanceOf(msg.sender) > 0, "must be token holder");
+        _;
+    }
+
     function createProposal(
         string memory _name,
-        uint256 _amount, 
+        uint256 _amount,
         address payable _recipient
-    ) external {
+    ) external onlyInvestor {
         require(address(this).balance >= _amount);
 
         proposalCount++;
 
         proposals[proposalCount] = Proposal(
-            proposalCount
-            ,_name, 
-            _amount, 
-            _recipient, 
-            0, 
-            false
-            );
-
-            emit Propose(
             proposalCount,
-             _amount, 
-             _recipient,
-             msg.sender);
+            _name,
+            _amount,
+            _recipient,
+            0,
+            false
+        );
 
-        
+        emit Propose(proposalCount, _amount, _recipient, msg.sender);
+    }
 
+    mapping(address => mapping(uint256 => bool)) votes;
+
+    function vote(uint256 _id) external onlyInvestor {
+        Proposal storage proposal = proposals[_id];
+
+        require(!votes[msg.sender][_id], "already voted");
+
+        proposal.votes += token.balanceOf(msg.sender);
+
+        votes[msg.sender][_id] = true;
+
+        emit Vote(_id, msg.sender);
+    }
+
+    function finalizeProposal(uint256 _id) external onlyInvestor {
+        Proposal storage proposal = proposals[_id];
+
+        require(proposal.finalized == false, "proposal already finalized");
+
+        proposal.finalized = true;
+
+        require(
+            proposal.votes >= quorum,
+            " must reach quorum to finalize proposal"
+        );
+
+        require(address(this).balance >= proposal.amount);
+
+        (bool sent, ) = proposal.recipient.call{value: proposal.amount}("");
+        require(sent);
+
+        emit Finalize(_id);
     }
 }
